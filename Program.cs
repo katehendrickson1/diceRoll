4f1e87a3673bd03022049529e38678629401c014
katehendrickson1 <119885479+katehendrickson1@users.noreply.github.com>
2024-01-17 21:23:43 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using MoreCSharpFun;
+public class Program
+{
+    static void Main(string[] args) // the start of the program
+    {
+
+        int numRolls = 0;
+
+        System.Console.WriteLine("Please enter the number of rolls");
+        numRolls = int.Parse(System.Console.ReadLine());
+
+        // Create an instance of the DiceRoll class
+        diceRoll diceRollInstance = new diceRoll(numRolls); //Declaration--making an instance of the diceRoll function
+                                                            // you need a constructor of numRolls bc that is what makes the instance unique
+
+        // Call the GetResults method to get the array of results
+        int[] results = diceRollInstance.getResults(); // int[] puts the results into an array 
+
+        for (int i = 2; i <= 12; i++) // i variable representing each possible combination of dice rolls starting at 2
+        {
+            int percentage = (int)Math.Round((double)results[i] / numRolls * 100); // calculate the percentage of rolls for each number and convert the first number to a double float
+            string output = new string('*', percentage); // make a new string variable that adds a * for each percentage point
+            Console.WriteLine($"{i}: {output} "); // concatinate the output and print it
+        }
+
+        System.Console.WriteLine("Thank you for using this cool dice rolling simulator!");
+    }
+
+
+}
+
+//if a class is static you can't create instances from it. 
---
+++ diceRoll.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreCSharpFun
+{
+    internal class diceRoll
+    {
+        int[] results = new int[13]; // Array to store counts of each possible combination (2 to 12)
+        public diceRoll(int numRolls)
+        {
+
+
+            Random random = new Random(); //import random class and make a random instance
+
+            for (int iCount=0; iCount < numRolls; iCount++ )
+            {
+                //generate 2 seperate random numbers for dice 1 and dice 2 
+                int dice1 = new Random().Next(1, 7);
+                int dice2 = new Random().Next(1, 7);
+
+                int combo = dice1 + dice2; //add dice rolls together
+
+                results[combo]++; // add results of the roll to reults array
+
+               
+
+            }
+        }
+        public int[] getResults()
+        {
+            return results; // a method that returns the results array 
+        }
+    }
+}
